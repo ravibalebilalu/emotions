c0e422472dcf398e63e4032b4c47421538800dfd
ravibalebilalu <81ravikiran@gmail.com>
2023-06-28 13:20:52 +0530

    model training

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -2,6 +2,9 @@ import os
 import sys
 import pandas as pd
 import numpy as np
+import re
+import nltk
+import pickle
 from dataclasses import dataclass
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -9,74 +12,88 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.compose import ColumnTransformer
 from src.logger import logging
 from src.exception import CustomException
-from src.helper import save_object
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+ps = SnowballStemmer(language = "english")
+nltk.download("stopwords")
+     
+
+def clean_text( text):         
+         
+        result = re.sub("[^a-zA-Z]"," ", text)
+         
+        result = result.lower()
+        result = result.split()
+        result = [ps.stem(word) for word in result if word not in set(stopwords.words('english'))]
+        result = " ".join(result)
+         
+        
+        return result
 
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path = os.path.join("artifacts","preprocessor.pkl")
+    vectorizer_obj_file_path = os.path.join("artifacts","tfidfvectorizer.pkl")
+    labelizer_obj_file_path = os.path.join("artifacts","labels.pkl")
 
 class DataTransformation:
     def __init__(self):
         self.data_transformation_config = DataTransformationConfig()
     
-    def get_data_transformation_obj(self):
-        try:
-            text_feature = "text"
-            target_feature = "emotions"
-
-            text_pipeline = Pipeline(
-                steps = [
-                    ("clean_text",clean_text()),
-                    ("tfidf",TfidfVectorizer())
-                ]
-                )
-            target_pipeline = Pipeline(
-                [("label_encoder",LabelEncoder())]
-                )
-            preprocessor = ColumnTransformer(
-                [("text_pipeline",text_pipeline,text_feature),
-                ("target_pipeline",target_pipeline,target_feature)]
-            )
-            return preprocessor
-        except Exception as e:
-            raise CustomException( e, sys)
     def initiate_data_transformation(self,train_path,test_path):
         try:
             train_df = pd.read_csv(train_path)
             test_df = pd.read_csv(test_path)
+            
             logging.info("Reading train and test data compleated")
 
             logging.info("Obtaining preprocessing object")
-            preprocessing_obj = self.get_data_transformation_obj()
+             
             target_column_name = "emotions"
-            text_feature_name = "text"
+            text_column_name = "text"
 
             input_feature_train_df = train_df.drop(columns = [target_column_name],axis = 1)
+            
             target_feature_train_df = train_df[target_column_name]
+
 
             input_feature_test_df = test_df.drop(columns = [target_column_name],axis = 1)
             target_feature_test_df = test_df[target_column_name]
+              
 
             logging.info("Applying preprocessing object on train and test data")
+            input_feature_train_df["text"] = input_feature_train_df['text'].apply(clean_text)
+            input_feature_test_df["text"] = input_feature_test_df['text'].apply(clean_text)
 
-            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df).toarray()
-            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df).toarray()
+            tf = TfidfVectorizer(max_features = 2500)
+            tf.fit(input_feature_train_df["text"])
+            vectorizer_obj = open(self.data_transformation_config.vectorizer_obj_file_path,"wb")
+            pickle.dump(tf,vectorizer_obj)
+            vectorizer_obj.close()
 
-            train_arr = np.array(input_feature_train_arr)
-            test_arr = np.array(input_feature_test_arr)
+            
 
-            logging.info("Saved preprocessing object")
-
-            save_object(self.data_transformation_config.preprocessor_obj_file_path, obj = preprocessing_obj)
-
+            train_transformed_arr = tf.transform(input_feature_train_df['text']).toarray()
+            test_transformed_arr = tf.transform(input_feature_test_df['text']).toarray()
+            
+             
+            le = LabelEncoder()
+            le.fit(target_feature_train_df)
+            labelizer = open(self.data_transformation_config.labelizer_obj_file_path,"wb")
+            pickle.dump(le, labelizer)
+            labelizer.close()
+            target_train_arr = le.transform(target_feature_train_df)
+            target_test_arr = le.transform(target_feature_test_df)
+             
+            
+            train_arr = np.c_[train_transformed_arr,target_train_arr]
+            test_arr = np.c_[test_transformed_arr,target_test_arr]
+                      
+             
             return (
                 train_arr,
-                test_arr,
-                self.data_transformation_config.preprocessor_obj_file_path
-            )
+                test_arr)
+                 
 
         except Exception as e:
             raise CustomException(e,sys)
-
-            
---
+++ src/components/model_training.py
@@ -0,0 +1,144 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+from dataclasses import dataclass
+from sklearn.metrics import accuracy_score,f1_score,precision_score
+from sklearn.linear_model import LogisticRegression,RidgeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier,AdaBoostClassifier
+from sklearn.model_selection import GridSearchCV
+from xgboost import XGBClassifier
+from catboost import CatBoostClassifier
+from src.exception import CustomException
+from src.logger import logging
+from src.helper import evaluate_models,load_object
+
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join("artifacts","model.pkl")
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self,train_arr,test_arr):
+        try:
+            logging.info("Splitting trainig and testing data")
+            x_train,y_train,x_test,y_test = (
+                train_arr[:,:-1],
+                train_arr[:,-1],
+                test_arr[:,:-1],
+                test_arr[:,-1]
+            )
+            models = {
+            "LogisticRegression" : LogisticRegression(),
+            "RidgeClassifier" : RidgeClassifier(),
+            "KNeighborsClassifier" : KNeighborsClassifier(),
+            "DecisionTreeClassifier" : DecisionTreeClassifier(),
+            "RandomForestClassifier" : RandomForestClassifier(),
+            "AdaBoostClassifier" : AdaBoostClassifier(),
+            "XgbClassifier" : XGBClassifier(),
+            "CatBoostClassifier" : CatBoostClassifier()
+            }
+            params = {
+               
+             "LogisticRegression":{
+            'penalty': ['l1', 'l2'],
+            'C': [0.001, 0.01, 0.1, 1.0, 10.0],
+            'fit_intercept': [True, False],
+            'solver': ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga'],
+            'max_iter': [100, 500, 1000],
+            'multi_class': ['auto', 'ovr', 'multinomial'],
+            'class_weight': [None, 'balanced']
+            },
+
+            "RidgeClassifier" :{
+                'alpha': [0.1, 1.0, 10.0],
+            'fit_intercept': [True, False],
+            'normalize': [True, False],
+            'solver': ['auto', 'svd', 'cholesky', 'lsqr', 'sparse_cg', 'sag', 'saga'],
+            'max_iter': [100, 500, 1000],
+            'class_weight': [None, 'balanced']
+            },
+            "KNeighborsClassifier" :{
+        
+            'n_neighbors': [3, 5, 7, 9],
+            'weights': ['uniform', 'distance'],
+            'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute'],
+            'leaf_size': [10, 20, 30, 40],
+            'p': [1, 2]
+            },
+            "DecisionTreeClassifier" :{
+     
+            'criterion': ['gini', 'entropy'],
+            'max_depth': [None, 5, 10, 20],
+            'min_samples_split': [2, 5, 10],
+            'min_samples_leaf': [1, 2, 4],
+            'max_features': [None, 'sqrt', 'log2']
+            },
+
+            "RandomForestClassifier":{
+     
+            'n_estimators': [100, 200, 500],
+            'criterion': ['gini', 'entropy'],
+            'max_depth': [None, 5, 10, 20],
+            'min_samples_split': [2, 5, 10],
+            'min_samples_leaf': [1, 2, 4],
+            'max_features': ['auto', 'sqrt', 'log2']
+            },
+            "AdaBoostClassifier": {
+     
+            'base_estimator': [DecisionTreeClassifier(max_depth=1), DecisionTreeClassifier(max_depth=2)],
+            'n_estimators': [50, 100, 200],
+            'learning_rate': [0.1, 0.5, 1.0],
+            'algorithm': ['SAMME', 'SAMME.R']
+            },
+            "XgbClassifier":{
+       
+            'max_depth': [3, 5, 7],
+            'learning_rate': [0.1, 0.01, 0.001],
+            'n_estimators': [100, 200, 300],
+            'gamma': [0, 0.1, 0.2],
+            'subsample': [0.8, 1.0],
+            'colsample_bytree': [0.8, 1.0],
+            'reg_alpha': [0, 0.1, 0.5],
+            'reg_lambda': [0, 0.1, 0.5],
+            'min_child_weight': [1, 5, 10]
+            },
+
+            "CatBoostClassifier" :{
+     
+            'depth': [6, 8, 10],
+            'learning_rate': [0.01, 0.05, 0.1],
+            'iterations': [30, 50, 100],
+            'l2_leaf_reg': [0.1, 0.5, 1.0],
+            'border_count': [32, 64, 128],
+            'bagging_temperature': [0.0, 1.0],
+            'random_strength': [0.0, 1.0],
+            'scale_pos_weight': [1.0, 2.0, 5.0]
+            }
+            }
+
+            model_report:dict = evaluate_models(x_train = x_train, y_train = y_train, x_test = x_test, y_test = y_test, models = models, param = params)
+            best_model_score = max(sorted(model_report.values()))
+
+            best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
+            best_model = models[best_model_name]
+
+            if best_model_score < 0.6:
+                raise CustomException("No best model found ")
+            logging.info("Best model found for both training and testing")
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,obj=best_model
+            )
+            predicted = best_model.predict(x_test)
+            score_ = accuracy_score(y_test,predicted)
+            return score_,best_model_name
+        except Exception as e:
+            raise CustomException( e, sys)
+            
+
